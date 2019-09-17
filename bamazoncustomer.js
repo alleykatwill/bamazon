c78eb32f756b1c77137757d82f26b1d7cce199cf
alleykatwill <48265377+alleykatwill@users.noreply.github.com>
2019-09-17 16:02:26 -0400

    Update bamazoncustomer.js

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -26,7 +26,7 @@ var display = function() {
       colWidths: [12, 50, 8],
       colAligns: ["center", "left", "right"],
       style: {
-        head: ["red"],
+        head: ["aqua"],
         compact: true
         // 'padding-right' : 1,
       }
@@ -106,107 +106,3 @@ var shopping = function() {
 };
 
 display();
-
-// //  NPM mySQL, inquirer, cli-table
-// var mysql = require('mysql');
-// var inquirer = require('inquirer');
-// var Table = require('cli-table');
-// var table = new Table({
-//   head: ['Item ID', 'product', 'Name', 'Price', 'Stock'],
-//   colWidths: [20, 40, 40, 10, 5]
-// });
-
-// // connection variable
-// var connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "",
-//   database: "bamazon_db"
-// });
-
-// connection.connect(function (err) {
-//   if (err) throw err;
-//   console.log("connected as id " + connection.threadId);
-// });
-
-// //Start the appliciton
-// letsShop();
-
-// //Displays products in database table
-// function letsShop() {
-//   connection.query('SELECT * FROM Products', function (err, res) {
-//     // Display products and price to user. Push the recordset from the DB into the CLI-table
-//     for (var i = 0; i < res.length; i++) {
-//       table.push([res[i].item_id, res[i].product_name, res[i].price.toFixed(2), res[i].stock_quantity])
-//     }
-//     console.log(table.toString());
-
-//     // Ask user questions for purchase
-//     inquirer.prompt([{
-//       // Ask user to choose a product to purchase
-//       name: "choice",
-//       type: "list",
-//       message: "What would you like to puchase today?",
-//       //This code will use the res object and grab the product names available.
-//       choices: function (value) {
-//         var choiceArray = [];
-//         for (var i = 0; i < res.length; i++) {
-//           choiceArray.push(res[i].product_name);
-//         }
-//         return choiceArray;
-//       }
-//     }, {
-//       // Ask user to enter a quantity to purchase
-//       name: "quantity",
-//       type: "input",
-//       message: "How many would you like to purchase?",
-//       validate: function (value) {
-//         if (isNaN(value) == false) {
-//           return true;
-//         } else {
-//           return false;
-//         }
-//       }
-//     }]).then(function (answer) {
-//       // Grabs the entire object for the product the user chose
-//       for (var i = 0; i < res.length; i++) {
-//         if (res[i].product_name == answer.choice) {
-//           var chosenItem = res[i];
-//         }
-//       }
-//       // Calculate remaining stock if purchase occurs
-//       var updateStock = parseInt(chosenItem.stock_quantity) - parseInt(answer.quantity);
-//       var pSales = parseFloat(chosenItem.product_sales).toFixed(2);
-
-//       // If customer wants to purchase more than available in stock, user will be asked if he wants to make another purchase
-//       if (chosenItem.stock_quantity < parseInt(answer.quantity)) {
-//         console.log(`${FgCyan} Insufficient quantity! ${FgWhite}`);
-//         repeat();
-//       }
-//       // If the customer wants to purchase an amount that is in stock, the remaining stock quantity will be updated in the database and the price presented to the customer
-
-//     }); // .then of inquirer prompt
-
-//   }); // first connection.query of the database
-
-// } // letsShop function
-
-// //Function used to make the experience of the CLI mode like a program. Provides an exit choice to the user.
-// function repeat() {
-//   inquirer.prompt({
-//     // Ask user if he wants to purchase another item
-//     name: "repurchase",
-//     type: "list",
-//     choices: ["Yes", "No"],
-//     message: "Would you like to purchase another item?"
-//   }).then(function (answer) {
-//     if (answer.repurchase == "Yes") {
-//       letsShop();
-//     }
-//     else {
-//       console.log(` Thanks for shopping with us. Have a great day! )
-//       connection.end();
-//     }
-//   });
-// }
